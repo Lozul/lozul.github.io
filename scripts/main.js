1ee2bb445464ada3ff0ac625f140a658c2559449
SejiKage <2037.game@laposte.net>
2017-12-02 18:49:37 +0100

    Redesign ;)

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
-
+// ~ Hello World ~
 console.log('%c<Hello World />', 'color: #DD375E');
+
+// Get elements
+
+var buttons = document.getElementsByClassName("buttons");
+
+var section = document.getElementsByTagName("section")[0];
+document.addEventListener('scroll', function(e) {
+
+  if ( window.scrollY >= section.offsetTop ) {
+    for (var i = 0; i < buttons.length; i ++) { buttons[i].style.visibility = "visible" }
+  } else {
+    for (var i = 0; i < buttons.length; i ++) { buttons[i].style.visibility = "hidden" }
+  }
+
+});
